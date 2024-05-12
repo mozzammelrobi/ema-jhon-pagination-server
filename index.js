bb7f9a07e6ac83e7788d944610e7fd4267d8be2e
mozzammelrobi <mozzammelfb@gmail.com>
2024-05-12 15:00:48 +0600

    pagination server

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-qhwzesf-shard-00-00.mrpyxhs.mongodb.net:27017,ac-qhwzesf-shard-00-01.mrpyxhs.mongodb.net:27017,ac-qhwzesf-shard-00-02.mrpyxhs.mongodb.net:27017/?ssl=true&replicaSet=atlas-4372m0-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,13 +28,38 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db('emaJohnDB').collection('products');
 
-    app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
-        res.send(result);
+    app.get('/products', async (req, res) => {
+      // console.log(req.query)
+      const page = parseInt(req.query.page)
+      const size = parseInt(req.query.size)
+      console.log(page, size)
+      const result = await productCollection.find()
+      .skip(page * size)
+      .limit(size)
+      .toArray();
+      res.send(result);
+    })
+
+    app.post('/productByIds', async(req, res ) => {
+      const ids = req.body;
+      const idsWithObjectId = ids.map(id => new ObjectId(id))
+      const query = {
+        _id: {
+          $in: idsWithObjectId
+        }
+      }
+      const result = await productCollection.find(query).toArray()
+      // console.log(idsWithObjectId)
+      res.send(result)
+    })
+
+    app.get('/productsCount', async(req, res) => {
+      const count = await productCollection.estimatedDocumentCount()
+      res.send({count})
     })
 
     // Send a ping to confirm a successful connection
@@ -46,10 +73,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send('john is busy shopping')
+app.get('/', (req, res) => {
+  res.send('john is busy shoppingggg')
 })
 
-app.listen(port, () =>{
-    console.log(`ema john server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`ema john server is running on port: ${port}`);
 })
